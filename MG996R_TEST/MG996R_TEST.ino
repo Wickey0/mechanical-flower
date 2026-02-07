2a5e339d42802fa803e87b6f9ec167992c9f0d99
Wickey0 <wickey8899@gmail.com>
2026-02-08 06:08:43 +0800

    Refactor servo control code and comments
    Updated comments for clarity and consistency.

--- MG996R_TEST/MG996R_TEST.ino
+++ MG996R_TEST/MG996R_TEST.ino
@@ -1,27 +1,26 @@
-#include <Servo.h>  // 引入舵机库
+#include <Servo.h>  
 
-Servo Servo_MG996R;      // 创建舵机对象
-const int servoPin = 2;  // 舵机信号线接D9
+Servo Servo_MG996R;      
+const int servoPin = 2;  // D2
 
 void setup() {
-  Servo_MG996R.attach(servoPin);  // 将舵机绑定到指定引脚
-  Servo_MG996R.writeMicroseconds(1500);  // 初始状态：停止
-  delay(1000);  // 等待舵机稳定
+  Servo_MG996R.attach(servoPin);  
+  Servo_MG996R.writeMicroseconds(1500);  // Initial state: stopped
+  delay(1000);  // Wait for the servo to stabilize 1000ms
 }
 
 void loop() {
-  // 正传（顺时针）：1000us脉宽（转速最快）
-  //Servo_MG996R.writeMicroseconds(1400);
-  //delay(30000);  // 正传20秒
   
-  // 停止1秒
-  //Servo_MG996R.writeMicroseconds(1500);
-  //delay(1000);
+  Servo_MG996R.writeMicroseconds(1400); //Flower Closed
+  delay(20000);  // 20,000ms 
+  
+  Servo_MG996R.writeMicroseconds(1500); //Flower Stop
+  delay(1000);
 
-  Servo_MG996R.writeMicroseconds(1200);
-  delay(30000);  // 正传20秒
+  Servo_MG996R.writeMicroseconds(1200); //Flower Open
+  delay(20000);  //20,000ms 
 
-  Servo_MG996R.writeMicroseconds(1500);
+  Servo_MG996R.writeMicroseconds(1500); //Flower Stop
   delay(1000);
 
 }
